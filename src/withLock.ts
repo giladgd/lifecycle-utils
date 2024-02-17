554afe3a2b4cb85b96e97f536860aa4f30491466
Gilad S <giladgd@gmail.com>
2024-02-17 02:23:26 +0200

    feat(minor): add abort signal support for locks

--- src/withLock.ts
+++ src/withLock.ts
@@ -3,13 +3,48 @@ const locks = new Map<any, Map<string, Promise<any>>>();
 /**
  * Only allow one instance of the callback to run at a time for a given `scope` and `key`.
  */
-export async function withLock<ReturnType>(scope: any, key: string, callback: () => Promise<ReturnType>): Promise<ReturnType> {
+export async function withLock<ReturnType>(scope: any, key: string, callback: () => Promise<ReturnType>): Promise<ReturnType>;
+export async function withLock<ReturnType>(
+    scope: any, key: string, acquireLockSignal: AbortSignal | undefined, callback: () => Promise<ReturnType>
+): Promise<ReturnType>;
+export async function withLock<ReturnType>(
+    scope: any,
+    key: string,
+    acquireLockSignalOrCallback: AbortSignal | undefined | (() => Promise<ReturnType>),
+    callback?: () => Promise<ReturnType>
+): Promise<ReturnType> {
+    let acquireLockSignal: AbortSignal | undefined = undefined;
+
+    if (acquireLockSignalOrCallback instanceof AbortSignal)
+        acquireLockSignal = acquireLockSignalOrCallback;
+    else if (acquireLockSignalOrCallback != null)
+        callback = acquireLockSignalOrCallback;
+
+    if (callback == null)
+        throw new Error("callback is required");
+
     while (locks.get(scope)?.has(key)) {
+        if (acquireLockSignal?.aborted)
+            throw acquireLockSignal.reason;
+
         try {
-            await locks.get(scope)?.get(key);
+            if (acquireLockSignal != null) {
+                const acquireLockPromise = createAbortSignalAbortPromise(acquireLockSignal);
+
+                await Promise.race([
+                    acquireLockPromise.promise,
+                    locks.get(scope)?.get(key)
+                ]);
+
+                acquireLockPromise.dispose();
+            } else
+                await locks.get(scope)?.get(key);
         } catch (err) {
             // we only need to wait here for the promise to resolve, we don't care about the result
         }
+
+        if (acquireLockSignal?.aborted)
+            throw acquireLockSignal.reason;
     }
 
     const promise = callback();
@@ -39,17 +74,20 @@ export function isLockActive(scope: any, key: string): boolean {
 /**
  * Acquire a lock for a given `scope` and `key`.
  */
-export async function acquireLock<S = any, K extends string = string>(scope: S, key: K): Promise<Lock<S, K>> {
+export async function acquireLock<S = any, K extends string = string>(
+    scope: S, key: K, acquireLockSignal?: AbortSignal
+): Promise<Lock<S, K>> {
     let releaseLock: (param: null) => void;
 
-    await new Promise((accept) => {
-        void withLock(scope, key, async () => {
+    await new Promise((accept, reject) => {
+        void withLock(scope, key, acquireLockSignal, async () => {
             accept(null);
 
             await new Promise((accept) => {
                 releaseLock = accept;
             });
-        });
+        })
+            .catch(reject);
     });
 
     return {
@@ -67,19 +105,38 @@ export async function acquireLock<S = any, K extends string = string>(scope: S, 
 /**
  * Wait for a lock to be released for a given `scope` and `key`.
  */
-export async function waitForLockRelease(scope: any, key: string): Promise<void> {
+export async function waitForLockRelease(scope: any, key: string, signal?: AbortSignal): Promise<void> {
     // eslint-disable-next-line no-constant-condition
     while (true) {
+        if (signal?.aborted)
+            throw signal.reason;
+
         try {
-            await locks.get(scope)?.get(key);
+            if (signal != null) {
+                const signalPromise = createAbortSignalAbortPromise(signal);
+
+                await Promise.race([
+                    signalPromise.promise,
+                    locks.get(scope)?.get(key)
+                ]);
+
+                signalPromise.dispose();
+            } else
+                await locks.get(scope)?.get(key);
         } catch (err) {
             // we only need to wait here for the promise to resolve, we don't care about the result
         }
+
+        if (signal?.aborted)
+            throw signal.reason;
 
         if (locks.get(scope)?.has(key))
             continue;
 
         await Promise.resolve(); // wait for a microtask to run, so other pending locks can be registered
+
+        if (signal?.aborted)
+            throw signal.reason;
 
         if (locks.get(scope)?.has(key))
             continue;
@@ -94,3 +151,37 @@ export type Lock<S = any, K extends string = string> = {
     dispose(): void,
     [Symbol.dispose](): void
 };
+
+function createControlledPromise<T = any>() {
+    let resolve: (value: T | Promise<T>) => void;
+    let reject: (reason?: any) => void;
+
+    const promise = new Promise<T>((accept, fail) => {
+        resolve = accept;
+        reject = fail;
+    });
+
+    return {
+        promise,
+        resolve: resolve!,
+        reject: reject!
+    };
+}
+
+function createAbortSignalAbortPromise(signal: AbortSignal) {
+    const acquireLockPromise = createControlledPromise<void>();
+
+    const onAbort = () => {
+        acquireLockPromise.resolve();
+        signal.removeEventListener("abort", onAbort);
+    };
+    signal.addEventListener("abort", onAbort);
+
+    return {
+        promise: acquireLockPromise.promise,
+        dispose() {
+            signal.removeEventListener("abort", onAbort);
+        }
+    };
+}
+
--- test/withLock.test.ts
+++ test/withLock.test.ts
@@ -50,6 +50,90 @@ describe("withLock", () => {
         expect(isLockActive(scope1, key1)).toBe(false);
     });
 
+    test("lock works with acquireLockSignal", async () => {
+        const scope1 = {};
+        const key1 = "key";
+
+        const waitForEnoughMicrotasks = async () => {
+            for (let i = 0; i < 10; i++)
+                await Promise.resolve();
+        };
+
+        let proceedLock1: ((value: "something") => void) | null = null;
+        let lock1Done = false;
+        let lock1Error: any = undefined;
+        const lock1Controller = new AbortController();
+        const lockPromise1 = withLock(scope1, key1, lock1Controller.signal, async () => {
+            const res = await new Promise((accept) => {
+                proceedLock1 = accept;
+            });
+            lock1Done = true;
+            return res;
+        })
+            .catch((error) => {
+                lock1Error = error;
+            });
+
+        let proceedLock2: ((value: "something2") => void) | null = null;
+        let lock2Done = false;
+        let lock2Error: any = undefined;
+        const lock2Controller = new AbortController();
+        withLock(scope1, key1, lock2Controller.signal, async () => {
+            const res = await new Promise((accept) => {
+                proceedLock2 = accept;
+            });
+            lock2Done = true;
+            return res;
+        })
+            .catch((error) => {
+                lock2Error = error;
+            });
+
+        expect(proceedLock1).not.toBeNull();
+        expect(proceedLock2).toBeNull();
+        expect(lock1Error).toBeUndefined();
+        expect(lock2Error).toBeUndefined();
+        expect(lock1Done).toBe(false);
+        expect(lock2Done).toBe(false);
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        lock1Controller.abort(new TestError());
+
+        expect(proceedLock1).not.toBeNull();
+        expect(proceedLock2).toBeNull();
+        expect(lock1Error).toBeUndefined();
+        expect(lock2Error).toBeUndefined();
+        expect(lock1Done).toBe(false);
+        expect(lock2Done).toBe(false);
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        lock2Controller.abort(new TestError());
+
+        expect(proceedLock1).not.toBeNull();
+        expect(proceedLock2).toBeNull();
+        expect(lock1Error).toBeUndefined();
+        expect(lock2Error).toBeUndefined();
+        expect(lock1Done).toBe(false);
+        expect(lock2Done).toBe(false);
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        await waitForEnoughMicrotasks();
+
+        expect(proceedLock1).not.toBeNull();
+        expect(proceedLock2).toBeNull();
+        expect(lock1Error).toBeUndefined();
+        expect(lock2Error).to.be.instanceof(TestError);
+        expect(lock1Done).toBe(false);
+        expect(lock2Done).toBe(false);
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        proceedLock1!("something");
+        await expect(lockPromise1).resolves.toBe("something");
+        expect(isLockActive(scope1, key1)).toBe(false);
+        expect(proceedLock2).toBeNull();
+        expect(lock2Done).toBe(false);
+    });
+
     test("acquireLock", async () => {
         const scope1 = {};
         const key1 = "key";
@@ -77,6 +161,32 @@ describe("withLock", () => {
 
         const lock2 = await lock2Promise;
         lock2[Symbol.dispose]();
+        await new Promise((accept) => setTimeout(accept, 0));
+
+        expect(isLockActive(scope1, key1)).toBe(false);
+    });
+
+    test("acquireLock with acquireLockSignal", async () => {
+        const scope1 = {};
+        const key1 = "key";
+
+        const lock1Controller = new AbortController();
+        const lock1Promise = acquireLock(scope1, key1, lock1Controller.signal);
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        lock1Controller.abort(new TestError());
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        const lock1 = await lock1Promise;
+        expect(isLockActive(scope1, key1)).toBe(true);
+
+        const lock2Controller = new AbortController();
+        const lock2Promise = acquireLock(scope1, key1, lock2Controller.signal);
+
+        lock2Controller.abort(new TestError());
+        await expect(lock2Promise).rejects.toBeInstanceOf(TestError);
+
+        lock1.dispose();
         await new Promise((accept) => setTimeout(accept, 0));
 
         expect(isLockActive(scope1, key1)).toBe(false);
@@ -155,4 +265,44 @@ describe("withLock", () => {
         await waitForEnoughMicrotasks();
         expect(lockReleased).toBe(true);
     });
+
+    test("waitForLockRelease with signal", async () => {
+        const scope1 = {};
+        const key1 = "key";
+
+        const lock1 = await acquireLock(scope1, key1);
+
+        const lockWithError2 = withLock(scope1, key1, async () => {
+            throw new Error("some error");
+        });
+
+        const lockReleasedSignal = new AbortController();
+
+        let lockReleased = false;
+        const lockReleasePromise = (async () => {
+            await waitForLockRelease(scope1, key1, lockReleasedSignal.signal);
+            lockReleased = true;
+        })();
+
+        expect(lockReleased).toBe(false);
+        lockReleasedSignal.abort(new TestError());
+
+        expect(lockReleased).toBe(false);
+        await expect(lockReleasePromise).rejects.toBeInstanceOf(TestError);
+        expect(lockReleased).toBe(false);
+
+        lock1.dispose();
+
+        try {
+            await lockWithError2;
+            expect.unreachable("lockWithError2 should throw");
+        } catch (err) {
+            // do nothing
+        }
+    });
 });
+
+
+class TestError extends Error {
+
+}
